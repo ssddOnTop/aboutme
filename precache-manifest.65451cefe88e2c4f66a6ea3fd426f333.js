940c1ace1b24f9898c795fe7fad7da6c31c21aad
ssddOnTop <ssddtbs@gmail.com>
2022-02-06 08:05:24 +0530

    Updates

---
+++ precache-manifest.65451cefe88e2c4f66a6ea3fd426f333.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130a5a3ab8d316ca4ee03e11887a0a96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f74847c9e84314b5bee9",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "d3f364c8a8a007d9db77",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "2e384d4486e6dfcec5e3",
+    "url": "/static/css/main.85ccaf56.chunk.css"
+  },
+  {
+    "revision": "95500218486d7f1b3ab4",
+    "url": "/static/js/2.81c54332.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.81c54332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74847c9e84314b5bee9",
+    "url": "/static/js/3.0d132fb5.chunk.js"
+  },
+  {
+    "revision": "d3f364c8a8a007d9db77",
+    "url": "/static/js/4.7c1262a0.chunk.js"
+  },
+  {
+    "revision": "2e384d4486e6dfcec5e3",
+    "url": "/static/js/main.2d703b3f.chunk.js"
+  },
+  {
+    "revision": "2816b3d8dccb02cdc48a",
+    "url": "/static/js/runtime-main.6eb638bf.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "59e428661ead7afda5946fd933856a5c",
+    "url": "/static/media/ssdd.59e42866.jpg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
